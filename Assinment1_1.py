0a6a2b7f8ce0c73b5401de3713811711716653ac
willsonger <98417168+willsonger@users.noreply.github.com>
2022-01-31 12:50:00 -0600

    Add files via upload

---
+++ Assinment1_1.py
@@ -0,0 +1,17 @@
+'''
+ Assignment 1.1
+ Discrription - Personal Information Write a program that displays 
+ the following information: Your name Your address, with city, state, 
+ and ZIP Your telephone number Your college majortion to Interactive 
+ Programming in Python 
+ Submitted by - William Songer
+ Course - COSC 2348 Introduction to Scripting
+ Instructor - Adarsh Kesireddy
+
+'''
+#Contact information
+print("   Student: William Songer")
+print("   Address: 3230 Hampton St.")
+print("            Corpus Christi, Texas 78414")
+print(" telephone: 361-739-8386")
+print("     Major: BS Computer Science, Cyber Security")
